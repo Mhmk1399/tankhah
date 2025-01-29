6cf583aaa5265521b54743ee811e86bd6b6666b4
mohamadhosein <mhmk1399@gmail.com>
2025-01-29 17:25:17 +0330

    sd

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,18 +1,19 @@
 import SideBar from "@/components/sideBar";
+import TransactionList from "@/components/transactionList";
 // import TransactionList from "@/components/transactionList";
 
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center ">
-      {/* <div className="space-y-4">
+      <SideBar />{" "}
+      <div className="space-y-4">
         <div id="income-list">
           <TransactionList type="incomes" />
         </div>
         <div id="outcome-list">
           <TransactionList type="outcomes" />
         </div>
-      </div> */}
-      <SideBar />
+      </div>
     </div>
   );
 };
